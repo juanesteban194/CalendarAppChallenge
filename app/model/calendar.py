a330fc38dbf47beb2d442f7e7c3c486e63b1d37f
estebanechavarria <juanestebanechavarria511@gmail.com>
2025-04-01 17:26:07 -0500

    punto 1 y 2

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,18 +1,62 @@
 from dataclasses import dataclass, field
 from datetime import datetime, date, time
+from math import remainder
 from typing import ClassVar
 
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
 
 
-# TODO: Implement Reminder class here
+@dataclass
+class Reminder:
+        EMAIL: ClassVar[str] = "email"
+        SYSTEM: ClassVar[str] = "system"
+        date_time: datetime
+        type: str = EMAIL
+
+        def __str__(self):
+            return f"Reminder on {self.date_time} of type {self.type}"
 
 
-# TODO: Implement Event class here
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default_factory = list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, type: str):
+        reminder = Reminder(date_time, type)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index:int):
+        if 0 < reminder_index < len(self.reminders):
+            self.reminders.pop(reminder_index)
+        else:
+            reminder_not_found_error()
 
 
-# TODO: Implement Day class here
 
 
-# TODO: Implement Calendar class here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
