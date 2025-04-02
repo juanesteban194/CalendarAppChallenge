47c90b7767a651fc258977707d68406d9de407c3
estebanechavarria <juanestebanechavarria511@gmail.com>
2025-04-02 07:24:58 -0500

    punto 3 y 4

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -33,11 +33,67 @@ class Event:
         self.reminders.append(reminder)
 
     def delete_reminder(self, reminder_index:int):
-        if 0 < reminder_index < len(self.reminders):
+        if 0 <= reminder_index < len(self.reminders):
             self.reminders.pop(reminder_index)
         else:
             reminder_not_found_error()
 
+    def __str__(self):
+            return f"ID: {self.id} Event title: {self.title} Description: {self.description} Time: {self.start_at} - {self.end_at}"
+
+
+class Day:
+    def __init__(self, date_: date, ):
+        self.date_: date = date_
+        self.slots: dict[time, str | None] = {}
+        self._init_slots()
+
+    def _init_slots(self):
+        self.slots  = {
+            time( minutos //60, minutos % 60): None
+            for minutos in range (0, 24 * 60, 15)
+                       }
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+            return event_id
+
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
+
+class Calendar:
+
+    def __init__(self):
+        self.days: dict[date, Day] = {}
+        self.events: dict[str, Event] = {}
+
+    def add_event(self, title: str, description: str, date_: date, start_at: time, end_at: time):
+        if date_ < datetime.now().date():
+            date_lower_than_today_error()
+        if date_ not in self.days:
+            self.days[date_] = Day(date_)
 
 
 
@@ -45,10 +101,7 @@ class Event:
 
 
 
-
-
-
-
+    
 
 
 
